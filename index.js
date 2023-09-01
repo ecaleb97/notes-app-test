76c18423b7a5e9d9698296b24750255c1e3f2ccc
Caleb <caleb@Calebs-MBP.home>
2023-09-01 13:52:20 +0200

    test

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
+import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import { Note } from './models/note.js'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+let notes = []
+
 app.use(express.json())
-
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
-  }
-]
-
 app.use(cors())
 app.use(express.static('dist'))
 
@@ -35,41 +17,37 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
+  Note.find({})
+    .then(notes => {
+      res.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    res.json(note)
-  } else {
-    res.status(404).end()
-  }
+  Note.findById(req.params.id)
+    .then(note => {
+      res.json(note)
+    })
 })
-
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map(note => note.id)) : 0
-  return maxId + 1
-}
 
 app.post('/api/notes', (req, res) => {
   const body = req.body
-  if (!body.content) {
+
+  if (body.content === undefined) {
     return res.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId()
-  }
+  const note = new Note(
+    {
+      content: body.content,
+      important: body.important || false
+    }
+  )
 
-  notes = notes.concat(note)
-
-  res.json(note)
+  note.save()
+    .then(savedNote => {
+      res.json(savedNote)
+    })
 })
 
 app.delete('/api/notes/:id', (req, res) => {
